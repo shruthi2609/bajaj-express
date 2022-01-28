54670ea515a5445f25e1e3da3539b48a2f1717d1
shruthi093 <jayasruthi.l@gmail.com>
2022-01-28 15:54:08 +0530

    validation

--- userModel.js
+++ userModel.js
@@ -3,15 +3,27 @@ const app=express()
 const mongoose=require("mongoose")
 mongoose.connect("mongodb+srv://cram_js:atlasdb@cluster0.zdsqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
 const User=mongoose.model("publications",{
-    author:String,
-    password:String,
-    userName:String
+   email:{
+    type:String,
+    unique:true,
+    required:[true,"please enter the email"],
+   },
+   userName:{
+    type:String,
+    minlength:[5,"please enter atleast 5 characters"],
+    maxlength:15
+   },
+   interest:{
+    type:[String],
+    enum:["react","node","express"],
+    required:true
+   }
 })
 const user1=new User(
     {
-        author:"authorabc",
-        password:"pass234",
-        userName:"testauthor"
+        email:"test123@gmail.com",
+        userName:"test12",
+        interest:["node"]
     }
 )
 user1.save().then(()=>console.log("user added successfully"))
