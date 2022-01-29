5c1c3bb2058425768cca474232587c7a229afa14
shruthi093 <jayasruthi.l@gmail.com>
2022-01-29 14:51:33 +0530

    jwt

--- routes/signinUser.js
+++ routes/signinUser.js
@@ -3,6 +3,7 @@ const router=express.Router()
 const logger=require("../middleware/logger")
 const User=require("../models/userModel")
 const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 const { findOne } = require("../models/userModel")
 router.post("/signin",async (req,res)=>{
     try{
@@ -12,12 +13,17 @@ router.post("/signin",async (req,res)=>{
     console.log(user.password)
     const authenticate=await bcrypt.compare(data.password,user.password)
     if(authenticate){
-        res.send("signed in successfully")
+        const token=jwt.sign({email:user.email},"jamesbond")
+        console.log(token)
+        const signinInfo={
+            msg:"successfuly signedin",
+            jwttoken:token
+        }
+        res.send(signinInfo)
     }
     else{
         res.send("Incorrect Password")
     }
-  
 }
 catch(err){
     res.send(err)
